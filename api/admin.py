d89c90f604c88b037719384d751ad865d9543de2
Sergey Medvedev <medvedevsm1989@yandex.ru>
2020-09-22 15:45:22 +0300

    added models

---
+++ api/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+from . import models
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(models.Category, CategoryAdmin)
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(models.Genre, GenreAdmin)
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'year', 'category')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(models.Title, TitleAdmin)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'text', 'author', 'score', 'pub_date')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(models.Review, ReviewAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'review', 'text', 'author', 'pub_date')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(models.Comment, CommentAdmin)
