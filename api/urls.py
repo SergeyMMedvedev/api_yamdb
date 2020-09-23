58365b9e825b4d76d9acc6847c67c6423711ea41
Andrey <and.isaev.v.@yandex.ru>
2020-09-23 14:12:49 +0300

    url, views, serializers for Review and Comment

---
+++ api/urls.py
@@ -0,0 +1,17 @@
+from rest_framework.routers import DefaultRouter
+from .views import (
+    ReviewViewSet
+)
+from django.urls import path, include
+
+
+router = DefaultRouter()
+router.register(r'titles/(?P<title_id>[0-9]+)/reviews',
+                ReviewViewSet, basename='reviews')
+router.register(r'titles/(?P<title_id>[0-9]+)/reviews/'
+                r'(?P<reviews_id>[0-9]+)/comments',
+                ReviewViewSet, basename='comments')
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Review
+from .serializers import ReviewSerializer, CommentSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class ReviewViewSet(ModelViewSet):
+    """Create, get, update reviews"""
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = []
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(ModelViewSet):
+    """Create, get, update comments for reviews"""
+    serializer_class = CommentSerializer
+    permission_classes = []
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        review = get_object_or_404(Review, id=self.kwargs['reviews_id'])
+        return review.comments.all()
