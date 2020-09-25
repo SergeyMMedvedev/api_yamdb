614eedadfc576aa728969712a578c13244b82df6
Sergey Medvedev <medvedevsm1989@yandex.ru>
2020-09-26 01:38:35 +0300

    all_tests_passed

--- api/views.py
+++ api/views.py
@@ -80,6 +80,18 @@ class CommentViewSet(ModelViewSet):
         review = get_object_or_404(Review, id=self.kwargs['reviews_id'])
         return review.comments.all()
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.check_exist()
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def check_exist(self):
+        get_object_or_404(Review, id=self.kwargs['reviews_id'])
+
+
 
 
 
